1065a6f1306e2b36d51ed2b3770b6d34c134dfac
Ywintersss <shawnpvp@gmail.com>
2024-04-24 23:57:26 +0800

    file read/write

---
+++ src/Project/Controller/Files.java
@@ -0,0 +1,107 @@
+package Project.Controller;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Files {
+    private static BufferedReader reader;
+    private static BufferedWriter writer;
+    public static String currentWorkingDirectory() {
+        return System.getProperty("user.dir");
+    }
+
+    //read txt file
+    public static ArrayList<String> readFile(String path) throws IOException {
+        if (!path.endsWith(".txt")){
+            throw new IllegalArgumentException("Invalid file type");
+        }
+        //initialize an array that stores the data
+        ArrayList<String> data = new ArrayList<>();
+
+        //try to open and read the file
+        try {
+            //create file reader
+            reader = new BufferedReader(new FileReader(path));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                //add each line to the array
+                data.add(line);
+            }
+
+            return data;
+
+        } finally {
+            //close the reader
+            if (reader != null) {
+                reader.close();
+            }
+        }
+    }
+
+    //parse txt file data <T> only accepts String or ArrayList<String>
+    public static<T> ArrayList<String[]> parseData(T data) {
+        //initialize an array that stores the data
+        ArrayList<String[]> parsedData = new ArrayList<>();
+
+        //parsing a list of data
+        if (data instanceof ArrayList<?>) {
+            for (Object obj : (ArrayList<?>) data) {
+                if (obj instanceof String) {
+                    String[] array = ((String) obj).split(",");
+                    parsedData.add(array);
+                }
+            }
+            //parsing single line data
+        } else if (data instanceof String) {
+            String str = (String) data;
+            String[] array = str.split(",");
+            parsedData.add(array);
+        } else {
+            throw new IllegalArgumentException("Invalid data type");
+        }
+
+
+        return parsedData;
+    }
+
+    //write to txt file
+    public static void writeToFile(String path, String data) throws IOException {
+        if (!(data instanceof String)) {
+            throw new IllegalArgumentException("Invalid data type");
+        }
+
+        try {
+            writer = new BufferedWriter(new FileWriter(path, true));
+
+            writer.write(data);
+            writer.newLine();
+
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
+    }
+
+    //update txt file
+    public static void updateFile(String path, String data) throws IOException {
+
+    }
+
+    //testing
+    public static void main(String[] args) {
+        try {
+            ArrayList<String> data = readFile(currentWorkingDirectory() + "\\src\\db\\Doctors.txtvs");
+            ArrayList<String[]> parsedData = parseData(data.get(0));
+            System.out.println(parsedData.get(0)[0]);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
